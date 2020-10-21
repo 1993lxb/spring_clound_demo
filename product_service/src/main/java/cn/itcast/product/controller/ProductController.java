86dbdf47ad52e4c4dbcc608258a12bcc0b1a968e
1993lxb <36787012+1993lxb@users.noreply.github.com>
2020-10-21 17:29:15 +0800

    first commit

---
+++ product_service/src/main/java/cn/itcast/product/controller/ProductController.java
@@ -0,0 +1,27 @@
+package cn.itcast.product.controller;
+
+
+import cn.itcast.product.entity.Product;
+import cn.itcast.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @RequestMapping(value = "/{id}",method = RequestMethod.GET)
+    public Product findById(@PathVariable Long id){
+
+        return productService.findById(id);
+    }
+    @RequestMapping(value = "",method = RequestMethod.POST)
+    public String  save(@RequestBody Product product){
+        productService.save(product);
+
+        return "保存成功";
+    }
+
+}
